55bcb95d59f53dcdaf134c7a47a76521177e7641
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2025-06-23 23:47:14 +0500

    added User Profile API

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -166,8 +166,35 @@ const verifyAuth = async (req, res) => {
   }
 };
 
+const getUserProfile = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log("Error getting user profile: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error while getting user profile",
+    });
+  }
+};
+
 module.exports = {
   signup,
   login,
   verifyAuth,
+  getUserProfile,
 };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -6,5 +6,6 @@ const { verifyToken } = require("../middlewares/auth.middleware");
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.get("/verify", verifyToken, authController.verifyAuth);
+router.get("/profile", verifyToken, authController.getUserProfile);
 
 module.exports = router;
